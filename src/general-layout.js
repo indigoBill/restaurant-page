9ae520a444b19069fc21b949ad389511d7e9acd5
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-01-15 09:14:55 -0500

    Create a function that animates the tab slider

--- src/general-layout.js
+++ src/general-layout.js
@@ -28,22 +28,33 @@ function createGeneralLayout(){
 
     function createPageTabs(){
         const tabSection = document.createElement('div');
+        const tabContainer = document.createElement('div');
         const homeTab = document.createElement('div');
         const menuTab = document.createElement('div');
         const contactTab = document.createElement('div');
+        const currTabSlider = document.createElement('div');
+        const tabAndSliderContainer = document.createElement('div');
 
         tabSection.classList.add('tab-section');
+        tabContainer.classList.add('tab-container');
         homeTab.classList.add('tab', 'home-tab');
         menuTab.classList.add('tab', 'menu-tab');
         contactTab.classList.add('tab', 'contact-tab');
+        currTabSlider.classList.add('tab-slider');
+        tabAndSliderContainer.classList.add('tabs-and-slider-container');
 
         homeTab.textContent = 'HOME';
         menuTab.textContent = 'MENU';
         contactTab.textContent = 'CONTACT';
 
-        tabSection.appendChild(homeTab);
-        tabSection.appendChild(menuTab);
-        tabSection.appendChild(contactTab);
+        tabContainer.appendChild(homeTab);
+        tabContainer.appendChild(menuTab);
+        tabContainer.appendChild(contactTab);
+
+        tabAndSliderContainer.appendChild(tabContainer);
+        tabAndSliderContainer.appendChild(currTabSlider);
+
+        tabSection.appendChild(tabAndSliderContainer);
 
         page.appendChild(tabSection);
     }
@@ -60,12 +71,32 @@ function createGeneralLayout(){
     createHomePageContent();
 }
 
+function moveTabSlider(){
+    const currentTab = document.querySelector('.current-tab');
+    const tabSlider = document.querySelector('.tab-slider');
+    const maxLength = 1;
+
+    if(tabSlider.classList.length > maxLength){
+        const removableClass = tabSlider.classList[maxLength];
+        tabSlider.classList.remove(removableClass);
+    }
+
+    if(currentTab.textContent === 'HOME'){
+        tabSlider.classList.add('tab-slider-home');
+    }else if(currentTab.textContent === 'MENU'){
+        tabSlider.classList.add('tab-slider-menu');
+    }else if(currentTab.textContent === 'CONTACT'){
+        tabSlider.classList.add('tab-slider-contact');
+    }
+}
+
 function changeCurrentTab(currentTab){
     const tabs = document.querySelectorAll('.tab');
 
     tabs.forEach((tab) => {
         if(tab.textContent === currentTab.toUpperCase()){
             tab.classList.add('current-tab');
+            moveTabSlider();
         }else{
             tab.classList.remove('current-tab');
         }
