baf16795f5832bcee603ab3966ab468be280b586
indigoBill <117851497+indigoBill@users.noreply.github.com>
2024-01-17 09:58:28 -0500

    Add content to modules

--- src/contact.js
+++ src/contact.js
@@ -1,12 +1,40 @@
-import { createContentContainer } from './general-layout.js';
+import { createContentContainer, adjustTabStyle } from './general-layout.js';
+import './style-sheets/contact.css';
 export { createContactPageContent };
 
 function createContactPageContent(){
     createContentContainer('contact');
+    adjustTabStyle('contact');
 
     const contentContainer = document.querySelector('.contact-content');
 
-    contentContainer.textContent = 'CONTACT PAGE';
+    const contactInfoContainer = document.createElement('div');
+    const phoneNumberContainer = document.createElement('div');
+    const phoneHeader = document.createElement('p');
+    const phoneNumber = document.createElement('p');
+    const addressContainer = document.createElement('div');
+    const addressHeader = document.createElement('p');
+    const address = document.createElement('p');
+
+    phoneHeader.textContent = '-PHONE NUMBER-';
+    phoneNumber.textContent = '(123)-456-7890';
+    addressHeader.textContent = '-ADDRESS-';
+    address.textContent = '123 Violet Pl, Brooklyn, NY, 11212';
+
+    contactInfoContainer.classList.add('contact-info-container');
+    phoneHeader.classList.add('contact-header');
+    addressHeader.classList.add('contact-header');
+
+    phoneNumberContainer.appendChild(phoneHeader);
+    phoneNumberContainer.appendChild(phoneNumber);
+
+    addressContainer.appendChild(addressHeader);
+    addressContainer.appendChild(address);
+
+    contactInfoContainer.appendChild(phoneNumberContainer);
+    contactInfoContainer.appendChild(addressContainer);
+
+    contentContainer.appendChild(contactInfoContainer);
 }
 
 
--- src/home.js
+++ src/home.js
@@ -1,11 +1,29 @@
-import { createContentContainer } from './general-layout.js';
+import { createContentContainer, adjustTabStyle } from './general-layout.js';
+import './style-sheets/home.css';
 export { createHomePageContent };
 
 function createHomePageContent(){
     createContentContainer('home');
+    adjustTabStyle('home');
 
     const contentContainer = document.querySelector('.home-content');
 
-    contentContainer.textContent = 'HOME PAGE';
+    const introContainer = document.createElement('div');
+    const introHeader = document.createElement('p');
+    const introSubtext = document.createElement('p');
 
+    introContainer.classList.add('introContainer');
+    introHeader.classList.add('intro-header');
+    introSubtext.classList.add('intro-subtext');
+
+    introHeader.textContent = '-Welcome!-';
+    introSubtext.textContent = 'Established in 2000, Island Flavor has been serving meals to its community for over 20 years. ';
+    introSubtext.textContent += 'What started as a dream has now become a reality and we hope to serve you for many years to come. ';
+    introSubtext.textContent += 'Take your time to view our menu and if anything peaks your interest, feel free to visit us!';
+
+
+    introContainer.appendChild(introHeader);
+    introContainer.appendChild(introSubtext);
+
+    contentContainer.appendChild(introContainer);
 }
